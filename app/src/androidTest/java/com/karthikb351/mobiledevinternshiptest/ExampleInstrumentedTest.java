746d8be954a57b081c86bc69d1a1974d6c26e1ba
gagangupta19 <gagan_gupta@live.in>
2016-10-12 00:48:37 +0530

    made some changes in travis.yml

--- app/src/androidTest/java/com/karthikb351/mobiledevinternshiptest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/karthikb351/mobiledevinternshiptest/ExampleInstrumentedTest.java
@@ -1,14 +1,20 @@
 package com.karthikb351.mobiledevinternshiptest;
 
+import android.app.Activity;
+import android.app.KeyguardManager;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.annotation.UiThreadTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.WindowManager;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.content.Context.KEYGUARD_SERVICE;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -36,6 +42,5 @@ public class ExampleInstrumentedTest {
         assertEquals("com.karthikb351.mobiledevinternshiptest", appContext.getPackageName());
 
         onView(withId(R.id.recyclerView)).check(matches(isDisplayed()));
-
     }
 }
