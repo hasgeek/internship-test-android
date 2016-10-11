d4beb33d0daead906ab0c85f51b501b73b083f91
Jishin <Jishin>
2016-10-11 23:14:48 +0530

    Assignment completed, git repository api is working with retrofit library and rx java as mentioned in assignment

--- app/src/main/java/com/karthikb351/mobiledevinternshiptest/network/APIService.java
+++ app/src/main/java/com/karthikb351/mobiledevinternshiptest/network/APIService.java
@@ -1,5 +1,7 @@
 package com.karthikb351.mobiledevinternshiptest.network;
 
+import com.karthikb351.mobiledevinternshiptest.helper.WebConstants;
+
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -11,13 +13,19 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIService {
 
+    Retrofit retrofit;
     GitHubApiInterface service;
 
-    public APIService() {
-        throw new RuntimeException("Build a Retrofit adapter from the GitHubApiInterface with RxJava Observable support as well a GSON parser");
-    }
-
     public GitHubApiInterface getService() {
+
+        retrofit  = new Retrofit.Builder().
+                addCallAdapterFactory(RxJavaCallAdapterFactory.create()).
+                addConverterFactory(GsonConverterFactory.create()).
+                baseUrl(WebConstants.END_URL).
+                build();
+
+        service = retrofit.create(GitHubApiInterface.class);
+
         return service;
     }
 }
