253bac63490728a0459e49b29b919438bb62f622
hackertronix <leavethemails@gmail.com>
2016-10-18 10:14:18 +0530

    mapped JSON response to model

--- app/src/main/java/com/karthikb351/mobiledevinternshiptest/model/Repository.java
+++ app/src/main/java/com/karthikb351/mobiledevinternshiptest/model/Repository.java
@@ -9,7 +9,46 @@ import com.google.gson.annotations.SerializedName;
 
 public class Repository {
 
-    public Repository() {
-        throw new RuntimeException("You need to fill out the fields in this class so GSON and Retrofit can bind to them");
+    //Mapping the JSON response to fields in the model
+
+    @SerializedName("id")
+    private String mId;
+    @SerializedName("name")
+    private String mName;
+    @SerializedName("full_name")
+    private String mFullName;
+    @SerializedName("description")
+    private String mDescription;
+
+    public Repository(String id, String name, String fullName, String description) {
+
+        //throw new RuntimeException("You need to fill out the fields in this class so GSON and Retrofit can bind to them");
+
+        mId = id;
+        mName = name;
+        mFullName = fullName;
+        mDescription = description;
     }
+
+    /*
+
+    THESE GETTERS WON'T BE USED IN THIS PROJECT SO WE'RE COMMENTING IT OUT
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }*/
+
+
+    public String getFullName() {
+        return mFullName;
+    }
+
 }
