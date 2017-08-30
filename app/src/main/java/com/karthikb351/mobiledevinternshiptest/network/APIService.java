a1c86d47cd0b8a4eff36e2ea9592aeee2589a828
kaushikthedeveloper <kaushikthedeveloper@gmail.com>
2017-08-30 21:34:18 +0530

    Completed the app with specified requirements implemented

--- app/src/main/java/com/karthikb351/mobiledevinternshiptest/network/APIService.java
+++ app/src/main/java/com/karthikb351/mobiledevinternshiptest/network/APIService.java
@@ -1,8 +1,13 @@
 package com.karthikb351.mobiledevinternshiptest.network;
 
+import com.karthikb351.mobiledevinternshiptest.model.Repository;
+
+import java.util.List;
+
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
 
 /**
  * Author: @karthikb351
@@ -12,12 +17,33 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class APIService {
 
     GitHubApiInterface service;
+    Observable<List<Repository>> observable;
+
+    String url = "hasgeek";
 
     public APIService() {
-        throw new RuntimeException("Build a Retrofit adapter from the GitHubApiInterface with RxJava Observable support as well a GSON parser");
+//        throw new RuntimeException("Build a Retrofit adapter from the GitHubApiInterface with RxJava Observable support
+//              as well a GSON parser");
+
+        //instance of Retrofit for making network calls
+        Retrofit retrofitAdapter = new Retrofit.Builder()
+                .baseUrl("http://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+
+        //join the instance
+        service = retrofitAdapter.create(GitHubApiInterface.class);
+
+        observable =  service.getReposByOrg(url);
     }
 
     public GitHubApiInterface getService() {
         return service;
+    }
+
+    //Provide access to the Observable
+    public Observable<List<Repository>> getObservable() {
+        return observable;
     }
 }
